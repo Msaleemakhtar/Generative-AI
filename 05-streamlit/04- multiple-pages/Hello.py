7c2cf2c1f4b537bd10604ab4c8c73b489b87b51b
Msaleemakhtar <saleemakhtar864@gmail.com>
2023-12-20 16:16:43 +0500

    multiple pages added

---
+++ 05-streamlit/04- multiple-pages/Hello.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Hello",
+    page_icon = "👋",
+)
+
+st.write("# Hello world , this is saleem")
+
+st.sidebar.success("select a demo above")
